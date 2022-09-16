fd0ca9ebe2ad502b1c447c537aceb129d731ba44
Kern Agustin <aguskern@gmail.com>
2022-09-15 23:36:46 -0300

    subiendo archivos del desafio

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import Cart from './components/Cart';
+import Footer from './components/Footer';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import ItemListContainer from './components/ItemListContainer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemDetailContainer from './components/ItemDetailContainer';
+import { useState } from 'react';
+import Test from './test/Test';
+import { CartProvider } from './context/CartContext'
+
+export default function App() {
+
+  const [color, setColor] = useState('dark');
+
+  return (
+    <ChakraProvider>
+      <CartProvider>
+        <BrowserRouter>
+          {/* NAVBAR */}
+          <NavBar />
+          <Routes>
+            <Route exact path='/' element={<ItemListContainer />} />
+            <Route exact path='/test' element={<Test />} />
+            <Route exact path='/categoria/:category' element={<ItemListContainer />} />
+            <Route exact path='/producto/:id' element={<ItemDetailContainer />} />
+            <Route exact path='/cart' element={<Cart />} />
+          </Routes>
+          {/* FOOTER */}
+          <Footer />
+        </BrowserRouter>
+      </CartProvider>
+    </ChakraProvider>
+  );
+}
---
+++ src/components/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useCart } from '../context/CartContext'
+
+export default function Cart() {
+  
+  const {cart} = useCart()
+  console.log(cart);
+  return (
+    <div>Cart</div>
+  )
+}
